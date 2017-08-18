4cc2793ffc92e3f138f783d6827384892102b6ad
Taylor Wilson <twilson@instructure.com>
2017-08-18 11:30:33 -0600

    Remove this cause it isn't used

--- src/api/constants.js
+++ src/api/constants.js
@@ -5,4 +5,3 @@ const config = yaml.safeLoad(fs.readFileSync('./config.yml'))
 export const CANVAS_DOMAIN = config['canvasDomain']
 export const DEVELOPER_KEY = config['developerKey']
 export const ACCOUNT_ADMIN = config['accountAdmin']
-export const RANDOM_COURSE_NAMES = config['course']['names']
